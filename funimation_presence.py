7da08c34ecdf1e95c1ffb328a053844e04f04f02
Christopher Goes <ghostofgoes@gmail.com>
2018-07-04 01:09:09 -0600

    Make logging work

--- funimation_presence.py
+++ funimation_presence.py
@@ -5,6 +5,7 @@ import logging
 import json
 import time
 import os.path
+import sys
 
 import pypresence
 from pypresence import Presence
@@ -18,7 +19,6 @@ import psutil
 # * Get the playing state (paused/playing)
 # * Link to open app
 # * Link to open the specific episode in the app
-
 
 # Want to follow Discord's recommendations as much as possible here
 # https://discordapp.com/developers/docs/rich-presence/best-practices
@@ -38,17 +38,17 @@ import psutil
 # Other useful information that might be excessive, but let's see if we can get it
 #   Season name
 
-# TODO: 
 # Some examples:
 #   Star Blazers 2202 Episode 6: 1757485 (English)
 #   Full Metal Panic Season 2 Episode 4: 1345116 (English)
 #   Full Metal Panic Season 2 Episode 7: 1345122 (English)
 #   Full Metal Panic Season 2 Episode 8: 1345124 (English)
 #   Full Metal Panic Season 2 Episode 9: 1345126 (English)
+#   Full Metal Panic Season 2 Episode 10: 1345128 (English)
 
 # I suspect the episode "id" is incremented by two per episode since there may be two audo languages, English and Japanese.
 # The app doesn't make it easy to switch languages, however, so don't have a easy way to validate this hypothesis.
-# Either way, we can get the language from the filename. 
+# Either way, we can get the language from the filename.
 
 def get_episode_id(proc):
     # Returns Episode ID and Language
@@ -61,7 +61,7 @@ def get_episode_id(proc):
             base = os.path.basename(file.path)
             parts = base.split('_')
             return parts[0], parts[1] # Episode ID, Language
-    # TODO: raise an error?
+    # TODO: raise an error instead of returning a empty tuple?
     return ()
 
 
@@ -69,32 +69,32 @@ def get_process(name='funimation'):
     for proc in psutil.process_iter():
         if name.lower() in proc.name().lower():
             logging.debug("Found process %s (PID: %d, Status: %s)", 
-            proc.name(), proc.pid, proc.status())
+                          proc.name(), proc.pid, proc.status())
             return proc
     logging.warning("Couldn't find process %s", name)
 
 
 def main():
-    logging.basicConfig()
+    logging.basicConfig(datefmt="%H:%M:%S", level=logging.DEBUG,
+                        format="%(asctime)s %(levelname)-7s %(name)-7s %(message)s")
+    logging.getLogger('asyncio').setLevel(logging.ERROR)
     with open('app_config.json') as f:
         app_config = json.load(f)
     client_id = app_config["client_id"]
-    secret = app_config["secret"]
+    # we probably don't need the secret for this
+    # secret = app_config["secret"]
 
-    RPC = Presence(client_id,pipe=0)  # Initialize the client class
+    RPC = Presence(client_id, pipe=0)  # Initialize the client class
     RPC.connect() # Start the handshake loop
     atexit.register(RPC.close)
 
     # Get the Funimation process
-    process = get_process('funimation')
-    # TODO: check for none
+    process = get_process('funimation')  # TODO: check for none
 
     # TODO: when I make it a service, just wait around for process to respawn when it dies
-
     while process.is_running():
         episode_id, language = get_episode_id(process)
-        print(episode_id)
-        print(language)
+        logging.debug("Episode ID: %s\tLanguage: %s", episode_id, language)
         time.sleep(10)
 
     # from pprint import pprint as pp
